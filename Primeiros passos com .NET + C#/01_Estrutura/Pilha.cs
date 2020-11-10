4745399df8615cb1d46f6544e5d46caee499620b
Eduardo Francisco <kurosaki.yslan@gmail.com>
2020-11-10 13:00:01 -0300

    Meu primeiro comite das aulas da Avanade Fullstack Developer

---
+++ Primeiros passos com .NET + C#/01_Estrutura/Pilha.cs
@@ -0,0 +1,38 @@
+using System;
+
+namespace _01_Estrutura
+{
+    public class Pilha
+    {
+
+        Posicao primeiro;
+
+        public void Empilha(object item)
+        {
+            primeiro = new Posicao(primeiro, item);
+        }
+
+        public object Desempilha(){
+            if (primeiro == null)
+            {
+                throw new invalidOperationException();
+            }
+            object reultado = primeiro.item;
+            primeiro = primeiro.proximo;
+            return resultado;        
+            }
+            class Posicao
+            {
+                public Posicao proximo;
+
+                public object item;
+
+                public Posicao(Posicao proximo, object item)
+                {
+                    this.proximo = proximo;
+                    this.item = item;
+                }
+            }
+        }
+
+    }
---
+++ Primeiros passos com .NET + C#/DIO_Inicio/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Esta e minha primeira aula de C#");
+            int number = 5;
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine(value: $"Bem vindo a minha primeira aula de C# {i}");
+            }
+        }
+       
+    }
+}
